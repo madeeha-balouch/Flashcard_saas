49d2df984924633dde2292ca0308ba70fcc9d947
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-08-16 12:51:03 +0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,30 @@
-import Link from "next/link"
-//import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { SignedOut, UserButton } from "@clerk/nextjs";
 
-export default function Header() {
+export default async function Header() {
+  const { userId } = auth();
 
-    return(
-        <div className="bg-green-500 text-neutral-100">
-            <div className="container mx-auto flex items-center justify-between py-4 text-black text-bold">
-                <Link href="/">Home</Link>
-                <div>
-                    <div className="flex gap-4 items-center">
-                        <Link href="/signup">Sign up</Link>
-                        <Link href="/signin">Sign in</Link>
-                    </div>
-                </div>
+  return (
+    <div className="bg-green-500 text-neutral-100">
+      <div className="container mx-auto flex items-center justify-between py-5 text-black text-bold">
+        <Link href="/">
+            <div className="text-bold text-black text-xl">Home</div>
+        </Link>
+        <div>
+          {userId ? (
+            <div className="flex gap-4 items-center">
+                <Link href="/about">About</Link>
+              <UserButton afterSignOutUrl="/"/>
             </div>
+          ) : (
+            <div className="flex gap-4 items-center">
+              <Link href="/signup">Sign up</Link>
+              <Link href="/signin">Sign in</Link>
+            </div>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
