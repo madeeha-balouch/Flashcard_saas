9d7dc4894b54c2f97612f722ac95a3d90de5dd37
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-08-16 12:20:31 +0400

    User Login Authentication

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,16 @@
 import Image from "next/image";
+import { auth, currentUser } from '@clerk/nextjs/server'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth()
+  const user = await currentUser()
+
+  //console.log(userId)
+  //console.log(user)
+
+  if(!userId){
+    return <div>!Unauthorised User</div>
+  }
   return (
     <div>
       <h1 className="font-bold text-center mt-10">Home Page</h1>
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,11 @@
 import Link from "next/link"
+//import { auth } from "@clerk/nextjs";
 
 export default function Header() {
+
     return(
-        <div className="bg-gray-600 text-neutral-100">
-            <div className="container mx-auto flex items-center justify-between py-4">
+        <div className="bg-green-500 text-neutral-100">
+            <div className="container mx-auto flex items-center justify-between py-4 text-black text-bold">
                 <Link href="/">Home</Link>
                 <div>
                     <div className="flex gap-4 items-center">
