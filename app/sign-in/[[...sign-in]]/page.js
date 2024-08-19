5f7f79426d78a2ed1acbb567a7c28f5bc99740af
Madeeha Balouch <madeehabalouch26@gmail.com>
2024-08-20 02:03:30 +0400

    merged jahez's part (clerk)

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,9 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
 
-export default function signInPage() {
-    
+export default function Page() {
   return (
-  <SignIn />
-  
+    <div className='flex items-center justify-center flex-col gap-10 text-center bg-[#C2CFB2] min-h-screen'>
+        <h1 className='text-4xl font-bold mt-10 text-[#4B4A67]'>Sign In to FlashCard SaaS</h1>
+        <SignIn />
+    </div>
   )
 }
